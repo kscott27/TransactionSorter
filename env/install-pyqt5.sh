5b820db9f6833a8f8548ab2b263ebecc688f585f
Kevin Scott <kmscott27@gmail.com>
2020-05-30 16:38:51 -0700

    Grooming pass

---
+++ env/install-pyqt5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install python3-pyqt5
+sudo apt-get install pyqt5-dev-tools
+sudo apt-get install qttools5-dev-tools
+
+pip3 install xlsxwriter
+pip3 install pandas
+pip3 install matplotlib
